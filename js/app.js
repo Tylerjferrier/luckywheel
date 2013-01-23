b9e4ceb03cc13e591ac617b37137ff5224ce4649
Vinh <vinh.nguyen@acronics.com>
2013-01-22 17:21:20 -0800

    WIP: CrafyJS

--- js/app.js
+++ js/app.js
@@ -1,119 +1,152 @@
 /*
 So we need to draw six times. Each time we draw two arc
-*/  
-		var colors = ["#B8D430", "#3AB745", "#029990", "#3501CB",
+*/
+
+var Roulette = function () {
+  this.colors = ["#B8D430", "#3AB745", "#029990", "#3501CB",
              "#2E2C75", "#673A7E", "#CC0071", "#F80120",
              "#F35B20", "#FB9A00", "#FFCC00", "#FEF200"];
-var restaraunts = ["Cutee", "Mini", "MiniPlus", "eeTee",
+  this.awards = ["Cutee", "Mini", "MiniPlus", "eeTee",
                    "beegee", "good luck", "happy new", "year",
                    "Sorry", "well done", "one", "more"];
 
-var startAngle = 0;
-var arc = Math.PI / 6;
-var spinTimeout = null;
+  this.startAngle = 0;
+  this.arc = Math.PI / 6;
+  this.spinTimeout = null;
 
-var spinArcStart = 10;
-var spinTime = 0;
-var spinTimeTotal = 0;
+  this.spinArcStart = 10;
+  this.spinTime = 0;
+  this.spinTimeTotal = 0;
 
-var ctx;
-var wheelRadius = 500;
+  this.wheelRadius = 500;
 
+  return this;
+}  
 
-function drawRouletteWheel() {
+Roulette.prototype.draw = function() {
   var canvas = document.getElementById("canvas");
-  if (canvas.getContext) {
-    var outsideRadius = wheelRadius / 4 * 3;
-    var textRadius = 300;
-    var insideRadius = wheelRadius / 4 * 2;
-    
-    ctx = canvas.getContext("2d");
-    ctx.clearRect(0,0,2 * wheelRadius, 2*wheelRadius);
+    if (canvas.getContext) {
+      var outsideRadius = this.wheelRadius / 4 * 3;
+      var textRadius = 300;
+      var insideRadius = this.wheelRadius / 4 * 2;
       
-    ctx.strokeStyle = "black";
-    ctx.lineWidth = 2;
-   
-    ctx.font = '30px Helvetica, Arial';
-   
-    for(var i = 0; i < 12; i++) {
-      var angle = startAngle + i * arc;
-      ctx.fillStyle = colors[i];
+      var ctx = this.ctx = canvas.getContext("2d");
+      ctx.clearRect(0,0,2 * this.wheelRadius, 2 * this.wheelRadius);
+        
+      ctx.strokeStyle = "black";
+      ctx.lineWidth = 2;
      
-      ctx.beginPath();
-      ctx.arc(wheelRadius, wheelRadius, outsideRadius, angle, angle + arc, false);
-      ctx.arc(wheelRadius, wheelRadius, insideRadius, angle + arc, angle, true);
+      ctx.font = '30px Helvetica, Arial';
+     
+      for(var i = 0; i < 12; i++) {
+        var angle = this.startAngle + i * this.arc;
+        ctx.fillStyle = this.colors[i];
+       
+        ctx.beginPath();
+        ctx.arc(this.wheelRadius, this.wheelRadius, outsideRadius, angle, angle + this.arc, false);
+        ctx.arc(this.wheelRadius, this.wheelRadius, insideRadius, angle + this.arc, angle, true);
 
-      ctx.stroke();
-      ctx.fill();
-     //break;
-      ctx.save();
-      ctx.shadowOffsetX = -1;
-      ctx.shadowOffsetY = -1;
-      ctx.shadowBlur    = 0;
-      ctx.shadowColor   = "rgb(220,220,220)";
+        ctx.stroke();
+        ctx.fill();
+       //break;
+        ctx.save();
+        ctx.shadowOffsetX = -1;
+        ctx.shadowOffsetY = -1;
+        ctx.shadowBlur    = 0;
+        ctx.shadowColor   = "rgb(220,220,220)";
+        ctx.fillStyle = "black";
+        ctx.translate(this.wheelRadius + Math.cos(angle + this.arc / 2) * textRadius,
+                      this.wheelRadius + Math.sin(angle + this.arc / 2) * textRadius);
+        ctx.rotate(angle + this.arc / 2 + Math.PI / 2);
+        var text = this.awards[i];
+        ctx.fillText(text, -ctx.measureText(text).width / 2, 0);
+        ctx.restore();
+      }
+     
+      //Arrow
       ctx.fillStyle = "black";
-      ctx.translate(wheelRadius + Math.cos(angle + arc / 2) * textRadius,
-                    wheelRadius + Math.sin(angle + arc / 2) * textRadius);
-      ctx.rotate(angle + arc / 2 + Math.PI / 2);
-      var text = restaraunts[i];
-      ctx.fillText(text, -ctx.measureText(text).width / 2, 0);
-      ctx.restore();
+      ctx.beginPath();
+      ctx.moveTo(this.wheelRadius - 4, this.wheelRadius - (outsideRadius + 5));
+      ctx.lineTo(this.wheelRadius + 4, this.wheelRadius - (outsideRadius + 5));
+      ctx.lineTo(this.wheelRadius + 4, this.wheelRadius - (outsideRadius - 5));
+      ctx.lineTo(this.wheelRadius + 9, this.wheelRadius - (outsideRadius - 5));
+      ctx.lineTo(this.wheelRadius + 0, this.wheelRadius - (outsideRadius - 13));
+      ctx.lineTo(this.wheelRadius - 9, this.wheelRadius - (outsideRadius - 5));
+      ctx.lineTo(this.wheelRadius - 4, this.wheelRadius - (outsideRadius - 5));
+      ctx.lineTo(this.wheelRadius - 4, this.wheelRadius - (outsideRadius + 5));
+      ctx.fill();
     }
-   
-    //Arrow
-    ctx.fillStyle = "black";
-    ctx.beginPath();
-    ctx.moveTo(wheelRadius - 4, wheelRadius - (outsideRadius + 5));
-    ctx.lineTo(wheelRadius + 4, wheelRadius - (outsideRadius + 5));
-    ctx.lineTo(wheelRadius + 4, wheelRadius - (outsideRadius - 5));
-    ctx.lineTo(wheelRadius + 9, wheelRadius - (outsideRadius - 5));
-    ctx.lineTo(wheelRadius + 0, wheelRadius - (outsideRadius - 13));
-    ctx.lineTo(wheelRadius - 9, wheelRadius - (outsideRadius - 5));
-    ctx.lineTo(wheelRadius - 4, wheelRadius - (outsideRadius - 5));
-    ctx.lineTo(wheelRadius - 4, wheelRadius - (outsideRadius + 5));
-    ctx.fill();
-  }
-}
-   
-function spin() {
-  spinAngleStart = Math.random() * 10 + 10;
-  spinTime = 0;
-  spinTimeTotal = Math.random() * 3 + 4 * 1000;
-  rotateWheel();
+};
+
+Roulette.prototype.spin = function() {
+  this.spinAngleStart = Math.random() * 10 + 10;
+  this.spinTime = 0;
+  this.spinTimeTotal = Math.random() * 3 + 4 * 1000;
+  this.rotateWheel();
 }
 
-function rotateWheel() {
-  spinTime += 10;
-  if(spinTime >= spinTimeTotal) {
-    stopRotateWheel();
+Roulette.prototype.rotateWheel = function() {
+  this.spinTime += 10;
+  if(this.spinTime >= this.spinTimeTotal) {
+    this.stopRotateWheel();
     return;
   }
-  var spinAngle = spinAngleStart - easeOut(spinTime, 0, spinAngleStart, spinTimeTotal);
-  startAngle += (spinAngle * Math.PI / 180);
-  drawRouletteWheel();
-  spinTimeout = setTimeout('rotateWheel()', 30);
+  var spinAngle = this.spinAngleStart - easeOut(this.spinTime, 0, this.spinAngleStart, this.spinTimeTotal);
+  this.startAngle += (spinAngle * Math.PI / 180);
+  this.draw();
+  this.spinTimeout = setTimeout('this.rotateWheel()', 30);
 }
 
-function stopRotateWheel() {
-  clearTimeout(spinTimeout);
+Roulette.prototype.stopRotateWheel = function() {
+  clearTimeout(this.spinTimeout);
   var degrees = startAngle * 180 / Math.PI + 90;
   var arcd = arc * 180 / Math.PI;
   var index = Math.floor((360 - degrees % 360) / arcd);
   ctx.save();
   ctx.font = 'bold 30px Helvetica, Arial';
-  var text = restaraunts[index]
+  var text = this.awards[index]
   ctx.fillText(text, wheelRadius - ctx.measureText(text).width / 2, wheelRadius + 10);
   ctx.restore();
 }
 
-function easeOut(t, b, c, d) {
+Roulette.prototype.easeOut = function(t, b, c, d) {
   var ts = (t/=d)*t;
   var tc = ts*t;
   return b+c*(tc + -3*ts + 3*t);
 }
 
+
 $(document).ready(function () {  
+  
   Crafty.init(1000, 1000);
   Crafty.canvas.init();
-  drawRouletteWheel();
+  
+  //the loading screen that will display while our assets load
+
+  Crafty.scene("loading", function () {
+    console.log('Loading the Main App');
+      //load takes an array of assets and a callback when complete
+      Crafty.load(["resource/imgs/arrow.png", "resource/imgs/bg.png", "resource/audio/spin.wav"], function () {
+          Crafty.scene("main"); //when everything is loaded, run the main scene
+      });
+
+      //black background with some loading text
+      Crafty.background("#000");
+      Crafty.e("2D, DOM, Text").attr({ w: 100, h: 20, x: 150, y: 120 })
+              .text("Loading")
+              .css({ "text-align": "center" });
+  });
+
+  Crafty.scene("main", function () {
+      console.log('Start the Main App');
+      generateWorld();
+  });
+
+  //automatically play the loading scene
+  Crafty.scene("loading");
 })
+
+function generateWorld() {
+  var r = new Roulette();
+  r.draw();
+}
