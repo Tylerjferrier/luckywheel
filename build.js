cc9541089e43826d238b3186a20898691ee608b6
Kurei <kurei@axcoto.com>
2013-02-07 16:22:52 -0800

    Add sample build.js from RequireJS

---
+++ build.js
@@ -0,0 +1,18 @@
+({
+    baseUrl: "./scripts",
+    paths: {
+        jquery: 'jquery.min',
+        bootstrap: 'bootstrap.min',
+        'underscore': 'underscore-min',
+        'backbone': 'backbone-min',
+        text: 'text',
+        i18n: 'i18n',
+        transform: 'jquery.transform',
+        buzz: 'buzz',
+        parse: 'parse-1.2.0.min',
+        'localStorage': 'backbone.localStorage-min',
+        easing: 'jquery.easing.1.3'
+    },
+    name: "main",
+    out: "script/script.min.js"
+})
